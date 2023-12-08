50cd791575569bc0867568cc8ec1a4e78f769cfb
Akas <phelina94@gmail.com>
2023-12-08 16:34:22 +0100

    add sqlite library

---
+++ database/database.h
@@ -0,0 +1,15 @@
+//
+// Created by Lina Phe on 08/12/2023.
+//
+
+#ifndef FARMINGCO_DATABASE_H
+
+#define FARMINGCO_DATABASE_H
+
+int createDatabase();
+
+
+
+
+
+#endif //FARMINGCO_DATABASE_H
--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 #include <SDL.h>
-int main(int argc, char **argv){
+#include "database/database.h"
 
+void exitWithError(const char*);
+
+int main(int argc, char **argv){
+    createDatabase();
+    return 0;
+}
+
+void exitWithError(const char* message) {
+    SDL_Log("ERREUR : %s : %s\n", message, SDL_GetError());
+    SDL_Quit();
+    exit(EXIT_FAILURE);
 }
