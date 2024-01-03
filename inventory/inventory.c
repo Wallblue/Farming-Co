37cc09574c13bef7a26a01aafc7b336433523f1a
Wallblue <nrb.busquet@gmail.com>
2024-01-03 15:05:11 +0100

    Add item_add() function

--- inventory/inventory.c
+++ inventory/inventory.c
@@ -23,7 +23,7 @@ void affectItem(Item* item, int id, const char* name, unsigned char quantity, co
     strcpy(item->itemType, type);
 }
 
-short item_find(int id, Inventory inventory){
+short item_find(int id, const Inventory inventory){
     short i;
     for(i = 0; i < INVENTORY_SIZE; i++)
         if(inventory[i].id == id)
@@ -31,7 +31,7 @@ short item_find(int id, Inventory inventory){
     return -1;
 }
 
-short firstEmptySlot(Inventory inventory){
+short firstEmptySlot(const Inventory inventory){
     short i;
     for(i = 0; i < INVENTORY_SIZE; i++)
         if(inventory[i].id == 0 && inventory[i].quantity == 0)
@@ -39,16 +39,21 @@ short firstEmptySlot(Inventory inventory){
     return -1;
 }
 
-unsigned char item_set(Item item, Inventory inventory){
+unsigned char item_set(const Item item, Inventory inventory){
     short index = firstEmptySlot(inventory);
-    if(index == -1) return 1; //If inventory is full
+    if(index == -1) return FAILURE; //If inventory is full
 
-    inventory[index].id = item.id;
-    inventory[index].quantity = item.quantity;
-    strcpy(inventory[index].itemName, item.itemName);
-    strcpy(inventory[index].sprite, item.sprite);
-    strcpy(inventory[index].itemType, item.itemType);
-    return 0;
+    affectItem(inventory + index, item.id, item.itemName, item.quantity, item.sprite, item.itemType);
+    return SUCCESS;
+}
+
+unsigned char item_add(Item item, Inventory inventory){
+    short index = item_find(item.id, inventory);
+    if(index == -1) //If item does not exist in this inventory
+        return item_set(item, inventory);
+    //but if it does
+    inventory[index].quantity += item.quantity;
+    return SUCCESS;
 }
 
 unsigned char save_inventory(Inventory inventory){
@@ -58,13 +63,13 @@ unsigned char save_inventory(Inventory inventory){
     fp = fopen("inventorySave.json", "w");
     if(fp == NULL) {
         fprintf(stderr, "\nError while saving inventory.\n");
-        return 1;
+        return FAILURE;
     }
     jsonString = jsonifyInventory(inventory);
     fwrite(jsonString, strlen(jsonString), 1, fp);
     fclose(fp);
     free(jsonString);
-    return 0;
+    return SUCCESS;
 }
 
 char* jsonifyInventory(Inventory inventory){
