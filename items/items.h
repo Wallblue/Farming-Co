8a554d18414c60ac1723f28bcaa5a0a3b3aef03d
Wallblue <nrb.busquet@gmail.com>
2024-01-09 17:41:39 +0100

    Rework and move struct Item and json items manipulation

--- items/items.h
+++ items/items.h
@@ -1,9 +1,26 @@
 #ifndef ITEMS_H
 #define ITEMS_H
 
+#define MAX_SPRITE_LEN 128
+#define MAX_STR 25
+#define DESC_MAX 255
+
+struct Item{
+    int id;
+    char name[MAX_STR];
+    char type[MAX_STR];
+    char description[DESC_MAX];
+    unsigned short energyBonus;
+    unsigned char ability;
+    unsigned char growTime;
+    char sprite[MAX_SPRITE_LEN];
+    unsigned char quantity;
+};
+typedef struct Item Item;
 
 //Functions :
-void addItemToDatabase();
-void addItemsToDatabase();
+unsigned char addItemToDatabase(int id);
+unsigned char addItemsToDatabase();
+
 
 #endif
