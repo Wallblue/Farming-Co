bd511e411d11658c8eba38ca687fcb93dec297b5
Wallblue <nrb.busquet@gmail.com>
2024-01-10 13:39:54 +0100

    Move sql script in a new init.sql file

---
+++ database/init.sql
@@ -0,0 +1,68 @@
+CREATE TABLE IF NOT EXISTS cooperative(
+        coopId INTEGER PRIMARY KEY AUTOINCREMENT,
+        coopName VARCHAR(25),
+        startTime DATETIME,
+        maxMembers INTEGER,
+        level INTEGER,
+        money INTEGER
+    );
+
+CREATE TABLE IF NOT EXISTS player(
+        playerId INTEGER PRIMARY KEY AUTOINCREMENT,
+        pseudo VARCHAR(25),
+        startDate DATETIME,
+        farmName VARCHAR(25),
+        timeInGame INTEGER,
+        maxEnergy INTEGER,
+        coopId INTEGER REFERENCES cooperative(coopId)
+     );
+
+CREATE TABLE IF NOT EXISTS object(
+        objectId INTEGER PRIMARY KEY AUTOINCREMENT,
+        x INTEGER,
+        y INTEGER,
+        w INTEGER,
+        h INTEGER,
+        name VARCHAR(25),
+        poseDate INT,
+        growDate INT,
+        sprite VARCHAR(128),
+        state INTEGER,
+        boosted BOOLEAN,
+        playerId INTEGER REFERENCES player(playerId)
+     );
+
+CREATE TABLE IF NOT EXISTS place(
+        placeId INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR(50),
+        type INTEGER
+     );
+
+CREATE TABLE IF NOT EXISTS npc(
+        npcId INTEGER PRIMARY KEY AUTOINCREMENT,
+        firstName VARCHAR(25),
+        lastName VARCHAR(25),
+        skin VARCHAR(128),
+        tradable BOOLEAN,
+        placeId INTEGER REFERENCES place(placeId)
+     );
+
+CREATE TABLE IF NOT EXISTS item(
+        itemId INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR(25),
+        type VARCHAR(25),
+        description TEXT,
+        energyBonus INTEGER,
+        ability INTEGER,
+        quantity INTEGER,
+        sprite VARCHAR(128),
+        growTime INTEGER,
+        ownerId INTEGER REFERENCES player(playerId),
+        npcId INTEGER REFERENCES npc(npcId)
+     );
+
+CREATE TABLE IF NOT EXISTS dialog(
+        dialogId INTEGER PRIMARY KEY AUTOINCREMENT,
+        content TEXT,
+        npcId INTEGER REFERENCES npc(npcId)
+    );
